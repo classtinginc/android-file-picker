de418a80c32f34c2e4412ac81cbfd22bd782144f
Saebinna Kwon <saebinna@classting.com>
2019-07-04 14:53:59 +0900

    feat(example): file-picker example 구현

--- app/src/main/java/com/classtinginc/file_picker/MainActivity.java
+++ app/src/main/java/com/classtinginc/file_picker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.classtinginc.file_picker;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FilePicker.with(MainActivity.this)
+                        .startActivityForResult(1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data != null) {
+            Log.e("filePicker", data.toString());
+        }
     }
 }
